9d0b3737a875843d80cc31baad863f2bb5e2d2ec
RaflinMadani <maydanirafli@gmail.com>
2025-10-25 22:28:03 +0700

    Update index.php
    damn

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Music Player Saya</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div id="app-container">
+        </div>
+
+    <script src="script.js"></script>
 </body>
 </html>
